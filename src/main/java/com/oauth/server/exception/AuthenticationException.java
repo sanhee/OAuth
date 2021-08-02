dd6a01aab25446ec3cacc940fbe9e2e03ae34b89
sanhee <68936@naver.com>
2021-08-03 03:47:33 +0900

    프로젝트 초기화

--- src/main/java/com/oauth/server/exception/AuthenticationException.java
+++
@@ -1,7 +0,0 @@
-package com.oauth.server.exception;
-
-public class AuthenticationException extends RuntimeException {
-    public AuthenticationException(String message) {
-        super(message);
-    }
-}
--- src/main/java/com/oauth/server/exception/GitHubException.java
+++
@@ -1,7 +0,0 @@
-package com.oauth.server.exception;
-
-public class GitHubException extends RuntimeException {
-    public GitHubException(String message) {
-        super(message);
-    }
-}
